0261c657fcd75c9c4f8255b26b0762ced78f07a8
Luxter77 <Luxter77@users.noreply.github.com>
2023-01-09 06:17:38 -0300

    IT DOWNLOADS

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # IMDB-STUFF
+
 ETL for IMDB's interface exports.
+
+## TODOs
+
+- This script should eventually migrate to more sophisticated libraries that can handle big data with more ease that the standard library, like pandas.
---
+++ src/pull.py
@@ -0,0 +1,18 @@
+from tqdm.auto import tqdm
+import requests as re
+import gzip
+import os
+
+IMDB_ENDPOINT = "https://datasets.imdbws.com/"
+IMDB_FILES = [
+    "name.basics.tsv.gz",
+    "title.akas.tsv.gz",
+    "title.basics.tsv.gz",
+    "title.crew.tsv.gz",
+    "title.episode.tsv.gz",
+    "title.principals.tsv.gz",
+    "title.ratings.tsv.gz",
+]
+
+for file in tqdm(IMDB_FILES, leave=False, desc="Downloading IMDB interface files"):
+    open(os.path.join("tmp", file), 'wb').write(re.get(IMDB_ENDPOINT + file).content)
